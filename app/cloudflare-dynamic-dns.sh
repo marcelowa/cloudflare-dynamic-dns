3562ff39e9d2bdf2f848640dfbaee569b1c15546
Marcelo Waisman <marcelo@cardinalops.com>
2021-01-02 00:23:16 +0200

    chore: update to alpine 3.12.3 and add dates to logs

--- app/cloudflare-dynamic-dns.sh
+++ app/cloudflare-dynamic-dns.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-echo "Fetching Current IP"
+echo "$(date) Fetching Current IP"
 CURRENT_IP=$(curl -s http://ipv4.icanhazip.com)
 
-echo "Fetching Zone: $ZONE ID"
+echo "$(date) Fetching Zone: $ZONE ID"
 ZONE_ID=$(curl -s "https://api.cloudflare.com/client/v4/zones?name=$ZONE" -H "X-Auth-Email: $EMAIL" -H "X-Auth-Key: $API_KEY" -H "Content-Type: application/json" | jq -j ".result[0].id")
 
-echo "Fetching Record: $HOST  Information"
+echo "$(date) Fetching Record: $HOST  Information"
 GET_RECORD_RESPONSE=$(curl -s "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?name=$HOST" -H "X-Auth-Email: $EMAIL" -H "X-Auth-Key: $API_KEY" -H "Content-Type: application/json")
 RECORD_ID=$(echo $GET_RECORD_RESPONSE | jq -j ".result[0].id")
 RECORD_TYPE=$(echo $GET_RECORD_RESPONSE | jq -j ".result[0].type")
@@ -14,11 +14,11 @@ RECORD_TTL=$(echo $GET_RECORD_RESPONSE | jq -j ".result[0].ttl")
 RECORD_PROXIED=$(echo $GET_RECORD_RESPONSE | jq -j ".result[0].proxied")
 RECORD_IP=$(echo $GET_RECORD_RESPONSE | jq -j ".result[0].content")
 
-echo "Checking if record IP: $RECORD_IP matches the current IP: $CURRENT_IP"
+echo "$(date) Checking if record IP: $RECORD_IP matches the current IP: $CURRENT_IP"
 if [ "$CURRENT_IP" == "$RECORD_IP" ]; then
-    echo "Current IP matches the record IP, no need to update."
+    echo "$(date) Current IP matches the record IP, no need to update."
 else
-  echo "Record IP does not match the current IP, attempting to update..."
+  echo "$(date) Record IP does not match the current IP, attempting to update..."
   
   UPDATE_PAYLOAD=$( jq -n \
     --arg zoneId $ZONE_ID \
@@ -33,13 +33,13 @@ else
   IS_SUCCESS=$(echo $UPDATE_RECORD_RESPONSE | jq -j ".success")
 
   if [ "$IS_SUCCESS" == true ]; then
-    echo "Updated."
+    echo "$(date) Updated."
   else
-    echo "Update failed."
-    echo "Request:"
+    echo "$(date) Update failed."
+    echo "$(date) Request:"
     echo "---"
     echo $UPDATE_PAYLOAD | jq .
-    echo "Response:"
+    echo "$(date) Response:"
     echo "---"
     echo $UPDATE_RECORD_RESPONSE | jq .
   fi
